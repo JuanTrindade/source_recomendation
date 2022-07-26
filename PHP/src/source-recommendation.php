4c734d4a99c5d7135729b010efe4e98f85ca63f8
unknown <Administrador@DESKTOP-IVFUHS6.eaglecargo.local>
2022-07-26 14:47:32 -0300

    First Commit

---
+++ PHP/src/source-recommendation.php
@@ -0,0 +1,16 @@
+<?php 
+    /**
+     * @link            https://github.com/JuanTrindade/source_recomendation
+     * @since           1.0.0
+     * @package         Source_Recomendation
+     * 
+     * @wordpress-plugin 
+     * Plugin Name: Source Recommendation
+     * Plugin URI: https://github.com/JuanTrindade/source_recomendation
+     * Description: Display a video recomendations from youtube
+     * Version: 1.0.0
+     * Author: Juan Trindade
+     * Domain Path: /languages/
+     */
+
+?>
